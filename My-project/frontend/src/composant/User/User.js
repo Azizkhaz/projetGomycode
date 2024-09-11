5fcf4045d3d648df9a4199310aa9f42abc2a42df
Azizkhaz <khalfaouia362@gmail.com>
2024-09-11 13:51:58 +0200

    reservation done

--- My-project/frontend/src/composant/User/User.js
+++ My-project/frontend/src/composant/User/User.js
@@ -2,21 +2,25 @@ import React from 'react'
 import './ButtonLog.css'
 import Homme from './Homme/Homme'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 function User({Logout , auth}) {
  console.log('auth=>',auth)
+
+ const authUser = useSelector(state=>state.auth)
+ console.log('authUser user account =>', authUser)
   return (
 <>
 <header>
       <nav className="navbar">
       <div className="navbar-brand">
         
-        <span>User</span>
+        <span>{authUser.name}</span>
       </div>
       <div className="navbar-links">
         <Link to='/user' className="navbar-link" >Home</Link>
         <Link to='/menuclist' className="navbar-link"> Menu </Link> 
         <Link></Link>
-        <Link  to={`/reservation/${auth._id}`} className="navbar-link">
+        <Link  to={`/reservation/${authUser._id}`} className="navbar-link">
           Reservation
         </Link>
         <a href="#testimonial" className="navbar-link">Reviews</a>
